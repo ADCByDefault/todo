e37de86c0d5ea88947edd891620f9fb553a50226
Swaran Singh <107933244+ADCByDefault@users.noreply.github.com>
2024-04-21 23:39:21 +0200

    ++toFinish:userModel

---
+++ models/userModel.js
@@ -0,0 +1,49 @@
+//Not done yet
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username is required!"],
+            unique: true,
+            validate: {
+                validator: function (value) {},
+                message: (props) => `${props.value} is not a valid username!`,
+            },
+            minlength: [3, "Username must be at least 3 characters!"],
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: [3, "Password must be at least 3 characters!"],
+            validate: {
+                validator: function (value) {},
+                message: (props) => `Password is not valid!`,
+            },
+        },
+        name: { type: String },
+        email: { type: String },
+        todos: [
+            {
+                //todo list
+            },
+        ],
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    const user = this;
+    user.password = await bcrypt.hash(user.password, saltRounds);
+    next();
+});
+
+const userModel = mongoose.model("user", userSchema);
+
+export default userModel;
